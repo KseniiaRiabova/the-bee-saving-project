14690bc3cb6702d802991c9ea1bfbf9e5ab5bcc1
NadiiaLashtun <flor.smile11@gmail.com>
2025-09-03 09:39:37 +0200

    Fix position of the hero image depending on the screen size

--- client/src/components/UI/MainHeroLanding.jsx
+++ client/src/components/UI/MainHeroLanding.jsx
@@ -1,27 +1,25 @@
-import hiveLogo from "../../assets/imgs/hive.png";
-import saveTextLogo from "../../assets/imgs/save_text_logo.png";
-import beesTextLogo from "../../assets/imgs/bees_text_logo.png";
+import hiveLogo from '../../assets/imgs/hive.png';
+import saveTextLogo from '../../assets/imgs/save_text_logo.png';
+import beesTextLogo from '../../assets/imgs/bees_text_logo.png';
 
 function MainLandingHero() {
   return (
-    <div className="h-[500px] relative md:h-[800px]">
-      <div className="  flex flex-col items-center justify-center">
+    <div className='max-w-4xl xl:max-w-5xl w-full mx-auto px-10 py-6 md:p-8  '>
+      <div className='flex flex-col '>
         <img
           src={saveTextLogo}
-          alt="Save"
-          className="z-10 absolute top-[10%] left-0 scale-75 md:top-[10%] md:left-[2%] md:scale-100 lg:top-[10%] lg:left-[2%] xl:left-[6%] dark:grayscale dark:invert"
+          alt='Save'
+          className='self-start object-contain z-10 max-h-[clamp(100px,12vw,14vh)] dark:grayscale dark:invert'
         />
-
         <img
           src={hiveLogo}
-          alt="Hive"
-          className="z-20 absolute top-[6%] left-0 scale-100 md:top-[0%] md:left-[3%] md:scale-100 lg:top-[0%] lg:left-[3%] xl:left-[8%]"
+          alt='Hive'
+          className='object-contain z-20 max-h-[clamp(280px,35vw,40vh)] -mt-6 md:-mt-10 mb-12'
         />
-
         <img
           src={beesTextLogo}
-          alt="Bees"
-          className="z-30 absolute top-[68%] left-0 scale-75 md:top-[67%] md:left-[26%] md:scale-100 lg:top-[67%] lg:left-[30%] xl:left-[40%] dark:grayscale dark:invert"
+          alt='Bees'
+          className='self-end object-contain z-30 max-h-[clamp(100px,12vw,14vh)] -mt-24 md:-mt-44 lg:-mt-48 md:mb-6 dark:grayscale dark:invert'
         />
       </div>
     </div>
