956bd520cd7ca916d7264e8e3c8b76581e308e21
NadiiaLashtun <flor.smile11@gmail.com>
2025-10-31 19:12:50 +0100

    Add to forms Close button component

--- client/src/components/UI/RequestForm/RequestFormModal.jsx
+++ client/src/components/UI/RequestForm/RequestFormModal.jsx
@@ -1,33 +1,34 @@
-import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { BACKEND_URL } from "../../configs/envConfig";
-import MapComponent from "./MapComponent";
-import FetchLocationData from "../../../lib/FetchLocationData";
-import useRequestStore from "../../../stores/useRequestStore";
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { BACKEND_URL } from '../../configs/envConfig';
+import MapComponent from './MapComponent';
+import FetchLocationData from '../../../lib/FetchLocationData';
+import useRequestStore from '../../../stores/useRequestStore';
+import { ButtonClose } from '../ButtonClose';
 
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet-geosearch/dist/geosearch.css";
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet-geosearch/dist/geosearch.css';
 
 export const RequestFormModal = ({ showModal, setShowModal }) => {
   const customLabelStyles =
-    "block mb-2 font-medium text-gray-900 dark:text-white";
+    'block mb-2 font-medium text-gray-900 dark:text-white';
   const customInputStyles =
-    "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500";
+    'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500';
 
   const { getAccessTokenSilently } = useAuth0();
   const { addRequest } = useRequestStore();
   const [errors, setErrors] = useState({});
 
   const [formData, setFormData] = useState({
-    title: "",
-    description: "",
-    latitude: "",
-    longitude: "",
-    city: "",
-    country: "",
-    contactNumber: "",
-    image: "",
+    title: '',
+    description: '',
+    latitude: '',
+    longitude: '',
+    city: '',
+    country: '',
+    contactNumber: '',
+    image: '',
     isActive: true,
     isAccepted: false,
   });
@@ -42,12 +43,12 @@ export const RequestFormModal = ({ showModal, setShowModal }) => {
             FetchLocationData(latitude, longitude, setFormData); // Already exists, sets formData
           },
           (error) => {
-            console.error("Error getting user location:", error);
+            console.error('Error getting user location:', error);
             // Optionally keep default location if error
           }
         );
       } else {
-        console.error("Geolocation is not supported by this browser.");
+        console.error('Geolocation is not supported by this browser.');
       }
     };
 
@@ -62,14 +63,14 @@ export const RequestFormModal = ({ showModal, setShowModal }) => {
         const accessToken = await getAccessTokenSilently();
 
         const response = await fetch(`${BACKEND_URL}/dashboard`, {
-          method: "GET",
+          method: 'GET',
           headers: {
             Authorization: `Bearer ${accessToken}`,
           },
         });
 
         if (!response.ok) {
-          throw new Error("Failed to get RESPONSE");
+          throw new Error('Failed to get RESPONSE');
         }
 
         if (response.ok) {
@@ -77,7 +78,7 @@ export const RequestFormModal = ({ showModal, setShowModal }) => {
           const data = parsedResponse.user;
           setFormData((prevData) => ({
             ...prevData,
-            contactNumber: data.metadata?.contactNumber || "",
+            contactNumber: data.metadata?.contactNumber || '',
           }));
         }
       } catch (e) {
@@ -118,16 +119,16 @@ export const RequestFormModal = ({ showModal, setShowModal }) => {
       const token = await getAccessTokenSilently();
 
       const response = await fetch(`${BACKEND_URL}/requests/create`, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(formData),
       });
 
       if (!response.ok) {
-        throw new Error("Failed to create request");
+        throw new Error('Failed to create request');
       }
 
       const data = await response.json();
@@ -135,7 +136,7 @@ export const RequestFormModal = ({ showModal, setShowModal }) => {
       setShowModal(false);
       setErrors({});
     } catch (error) {
-      console.error("Error creating request:", error);
+      console.error('Error creating request:', error);
       setErrors({ submit: true });
     }
   };
@@ -148,14 +149,7 @@ export const RequestFormModal = ({ showModal, setShowModal }) => {
             <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
               <div className='flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t'>
                 <h3>Request Details</h3>
-                <button
-                  className='p-0 rounded-full bg-transparent border-0 float-right'
-                  onClick={() => setShowModal(false)}
-                >
-                  <span className='text-black h-6 w-6 block bg-gray-400 rounded-full'>
-                    X
-                  </span>
-                </button>
+                <ButtonClose onClose={() => setShowModal(false)} />
               </div>
 
               <div className='relative bg-white rounded-lg dark:bg-gray-700'>
@@ -216,7 +210,7 @@ export const RequestFormModal = ({ showModal, setShowModal }) => {
                       <MapContainer
                         center={markerPosition}
                         zoom={11}
-                        style={{ width: "100%", height: "300px" }}
+                        style={{ width: '100%', height: '300px' }}
                       >
                         <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
                         <MapComponent
