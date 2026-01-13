ae10c41a398cf6dff186b4869f668fa735114ec6
Nisha <nishavijai@gmail.com>
2026-01-13 16:10:01 +0100

    Fix lighthouse - dashboard (request form) - full score on light and dark theme

--- client/src/components/UI/ButtonClose.jsx
+++ client/src/components/UI/ButtonClose.jsx
@@ -1,13 +1,20 @@
+import PropTypes from 'prop-types';
+
 export const ButtonClose = ({ onClose }) => {
   return (
     <button
       className='btn-outline rounded-full p-2.5 group'
       onClick={onClose}
+      aria-label="Close"
     >
       <div className='relative w-2 h-2 flex justify-center items-center'>
-        <div className='absolute h-1 w-4 bg-primary-dark rounded-lg rotate-45 group-hover:bg-primary'></div>
-        <div className='absolute h-1 w-4 bg-primary-dark rounded-lg -rotate-45 group-hover:bg-primary'></div>
+        <div className='absolute h-1 w-4 bg-primary-dark rounded-lg rotate-45 group-hover:bg-secondary-dark group-focus:bg-secondary-dark dark:group-hover:bg-primary-dark dark:group-focus:bg-primary-dark'></div>
+        <div className='absolute h-1 w-4 bg-primary-dark rounded-lg -rotate-45 group-hover:bg-secondary-dark  group-focus:bg-secondary-dark dark:group-hover:bg-primary-dark dark:group-focus:bg-primary-dark'></div>
       </div>
     </button>
   );
+};
+
+ButtonClose.propTypes = {
+  onClose: PropTypes.func.isRequired,
 };
