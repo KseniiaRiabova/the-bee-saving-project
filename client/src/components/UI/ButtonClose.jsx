03aac184de269e3aa2803889e4ba1b4f341b431b
Nisha <nishavijai@gmail.com>
2026-02-04 14:12:15 +0100

    Update ButtonClose - rotate

--- client/src/components/UI/ButtonClose.jsx
+++ client/src/components/UI/ButtonClose.jsx
@@ -8,7 +8,7 @@ export const ButtonClose = ({ onClose }) => {
       aria-label="Close"
     >
       <div className='relative w-2 h-2 flex justify-center items-center'>
-        <div className='absolute h-1 w-4 bg-primary-dark rounded-lg -rotate-45 group-hover:bg-primary'></div>
+        <div className='absolute h-1 w-4 bg-primary-dark rounded-lg rotate-45 group-hover:bg-primary'></div>
         <div className='absolute h-1 w-4 bg-primary-dark rounded-lg -rotate-45 group-hover:bg-primary'></div>
       </div>
     </button>
