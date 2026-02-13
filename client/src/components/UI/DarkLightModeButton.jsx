ada1e1bffcdff37aeb08234a0e86153f6c9d65f6
Nadiia Lashtun <flor.smile11@gmail.com>
2026-02-13 12:07:54 +0100

    Add hover and focus to Dark mode button

--- client/src/components/UI/DarkLightModeButton.jsx
+++ client/src/components/UI/DarkLightModeButton.jsx
@@ -4,9 +4,7 @@ export const DarkLightModeButton = () => {
   const { isDarkMode, toggleDarkMode } = useDarkModeStore();
 
   return (
-    <label
-      className='relative block w-12 h-6'
-    >
+    <label className='relative block w-12 h-6'>
       <input
         type='checkbox'
         role='switch'
@@ -18,7 +16,7 @@ export const DarkLightModeButton = () => {
       />
 
       {/* Toggle track */}
-      <span className='absolute inset-0 bg-transparent border border-primary-dark rounded-full cursor-pointer'></span>
+      <span className='absolute inset-0 bg-transparent border border-primary-dark rounded-full hover:shadow-lg peer-focus-visible:outline peer-focus-visible:outline-1  duration-500 cursor-pointer'></span>
 
       {/* Toggle thumb */}
       <span className='absolute top-1 left-1 h-4 w-4 bg-primary border border-primary-dark rounded-full cursor-pointer transition-transform duration-300 peer-checked:translate-x-6 peer-checked:bg-primary'></span>
